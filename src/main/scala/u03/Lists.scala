ef013d9cad14cc5b4fc77bb8afc351c87508067e
Riccardo Bacca <riccardo.bacca@studio.unibo.it>
2022-03-17 15:22:12 +0100

    Implemented exercises from 1 to 4 about lists

--- src/main/scala/u03/Lists.scala
+++ src/main/scala/u03/Lists.scala
@@ -1,5 +1,13 @@
 package u03
 
+import u02.AlgebraicDataTypes.Person
+import u02.AlgebraicDataTypes.Person.Teacher
+import u02.Optionals.Option.*
+import u03.Lists.List.max
+
+import scala.annotation.tailrec
+
+
 object Lists extends App:
 
   // A generic linkedlist
@@ -17,14 +25,49 @@ object Lists extends App:
       case Cons(h, t) => Cons(mapper(h), map(t)(mapper))
       case Nil() => Nil()
 
+    def map2[A, B](l: List[A])(mapper: A => B): List[B] = l match
+      case Cons(h, t) => flatMap(l)(f => Cons(mapper(f), Nil()))
+      case Nil() => Nil()
+
     def filter[A](l1: List[A])(pred: A => Boolean): List[A] = l1 match
       case Cons(h, t) if pred(h) => Cons(h, filter(t)(pred))
       case Cons(_, t) => filter(t)(pred)
       case Nil() => Nil()
 
-  val l = List.Cons(10, List.Cons(20, List.Cons(30, List.Nil())))
-  println(List.sum(l)) // 60
+    def filterMap[A](l1: List[A])(pred: A => Boolean): List[A] = l1 match
+      case Cons(h, t) => flatMap(l1)(f => if pred(f) then Cons(f, Nil()) else Nil())
+      case Nil() => Nil()
 
-  import List.*
+    @tailrec
+    def drop[A](l: List[A], n: Int): List[A] = l match
+      case Cons(h,t) if n == 1 => t
+      case Cons(h,t) if n > 1 => drop(t, n-1)
+      case Nil() | Cons(_, _) => Nil()
 
-  println(sum(map(filter(l)(_ >= 20))(_ + 1))) // 21+31 = 52
+    def append[A](left: List[A], right: List[A]): List[A] = left match
+      case Cons(h, t) => Cons(h, append(t, right))
+      case Nil() => right
+
+    def flatMap[A,B](l: List[A])(f: A => List[B]): List[B] = l match
+      case Cons(h, t) => append(f(h), flatMap(t)(f))
+      case Nil() => Nil()
+
+    def max(l: List[Int]): u02.Optionals.Option[Int] = l match
+      case Cons(h, t) => max(t) match
+        case None() => Some(h)
+        case _ => if (orElse(max(t), 0) > h) max(t) else Some(h)
+      case Nil() | Cons(_, _) => None()
+
+    @tailrec
+    def foldLeft(l: List[Int])(init: Int)(fold: (Int, Int) => Int): Int = l match
+      case Cons(h, t) => foldLeft(t)(fold(init, h))(fold)
+      case Nil() => init
+
+    @tailrec
+    def foldRight(l: List[Int])(init: Int)(fold: (Int, Int) => Int): Int = l match
+      case Cons(h, t) => foldRight(t)(fold(-h, init))(fold)
+      case Nil() => init
+
+    def getCourses(persons: => List[Person]): List[String] = persons match
+      case Cons(h, t) => map(filter(persons)(a => a.isInstanceOf[Teacher]))(f => f.asInstanceOf[Teacher].course)
+      case Nil() => Nil()
--- src/test/scala/u03/ListTest.scala
+++ src/test/scala/u03/ListTest.scala
@@ -3,20 +3,49 @@ package u03
 import org.junit.*
 import org.junit.Assert.*
 import Lists.*
+import u02.AlgebraicDataTypes.Person
+import u02.AlgebraicDataTypes.Person.Teacher
+import u02.Optionals.Option.*
 
 class ListTest:
   import List.*
 
   val l: List[Int] = Cons(10, Cons(20, Cons(30, Nil())))
 
-  @Test def testSum() =
+  @Test def testSum(): Unit =
     assertEquals(0, sum(Nil()))
     assertEquals(60, sum(l))
 
-  @Test def testMap() =
+  @Test def testMap(): Unit =
+    assertEquals(Cons(11, Cons(21, Cons(31, Nil()))), map2(l)(_ + 1))
     assertEquals(Cons(11, Cons(21, Cons(31, Nil()))), map(l)(_ + 1))
-    assertEquals(Cons("10", Cons("20", Cons("30", Nil()))), map(l)(_ + ""))
+    assertEquals(Cons("10", Cons("20", Cons("30", Nil()))), map2(l)(_ + ""))
 
-  @Test def testFilter() =
+  @Test def testFilter(): Unit =
     assertEquals(Cons(20, Cons(30, Nil())), filter(l)(_ >= 20))
     assertEquals(Cons(10, Cons(30, Nil())), filter(l)(_ != 20))
+
+  @Test def testFilterMap(): Unit =
+    assertEquals(Cons(20, Cons(30, Nil())), filterMap(l)(_ >= 20))
+    assertEquals(Cons(10, Cons(30, Nil())), filterMap(l)(_ != 20))
+
+  @Test def testDrop(): Unit =
+    assertEquals(Cons(20, Cons(30, Nil())), drop(l, 1))
+    assertEquals(Cons(30, Nil()), drop(l, 2))
+    assertEquals(Nil(), drop(l, 5))
+
+  @Test def testAppend(): Unit =
+    val tail = Cons(40, Nil())
+    assertEquals( Cons(10, Cons(20, Cons(30, Cons(40, Nil())))), append(l, tail))
+
+  @Test def testFlatMap(): Unit =
+    assertEquals(Cons(11, Cons(21, Cons(31, Nil()))), flatMap(l)(v => Cons(v + 1, Nil())))
+    assertEquals(Cons(11, Cons(12, Cons(21, Cons(22, Cons(31, Cons(32, Nil())))))), flatMap(l)(v => Cons(v + 1, Cons(v + 2, Nil()))))
+
+  @Test def testMax(): Unit =
+    assertEquals(Some(25), max(Cons(10, Cons(25, Cons(20, Nil())))))
+    assertEquals(None(), max(Nil()))
+
+  @Test def testGetCourses(): Unit =
+    val teachers: List[Person] = Cons(Teacher("mario", "algebra"), Cons(Teacher("paolo", "analysis"), Nil()))
+    assertEquals(Cons("algebra", Cons("analysis",Nil())), getCourses(teachers))
